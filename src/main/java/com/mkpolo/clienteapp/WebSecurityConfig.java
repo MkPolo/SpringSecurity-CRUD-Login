9e8e08d3735b3ff00150004a8976468278b1c12f
MkPolo <mk.polo1708@gmail.com>
2020-04-26 19:53:29 -0500

    Se implementó las Autorizaciones HTTP

--- src/main/java/com/mkpolo/clienteapp/WebSecurityConfig.java
+++ src/main/java/com/mkpolo/clienteapp/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -17,6 +18,26 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 	@Autowired
 	private BCryptPasswordEncoder passEncoder;
 	
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		//usamos el atributo http para indicar que sean permitidas todas las solicitudes de acuerdo al rol del usuario
+		http.authorizeRequests()
+		.antMatchers("/","/css/**","/js/**","/images/**").permitAll()
+		.antMatchers("/views/clientes/").hasAnyRole("USER")
+		.antMatchers("/views/clientes/").hasAnyRole("ADMIN")
+		.antMatchers("/views/clientes/create").hasAnyRole("ADMIN")
+		.antMatchers("/views/clientes/save").hasAnyRole("ADMIN")
+		.antMatchers("/views/clientes/edit/**").hasAnyRole("ADMIN")
+		.antMatchers("/views/clientes/delete/**").hasAnyRole("ADMIN")
+		.anyRequest().authenticated()
+		.and()
+		.formLogin().permitAll()
+		.and()
+		.logout().permitAll();
+	}
+
+
 	@Autowired
 	public void cofigurerSecurityGlobal(AuthenticationManagerBuilder builder) throws Exception {
 		builder.jdbcAuthentication()
