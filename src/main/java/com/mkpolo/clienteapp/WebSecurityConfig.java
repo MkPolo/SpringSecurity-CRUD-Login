a77e741da951161cc021180458bf203265babd65
MkPolo <mk.polo1708@gmail.com>
2020-04-26 19:19:46 -0500

    Se implementó la autenticación jdbc

---
+++ src/main/java/com/mkpolo/clienteapp/WebSecurityConfig.java
@@ -0,0 +1,28 @@
+package com.mkpolo.clienteapp;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Autowired
+	private DataSource dataSource;
+	
+	@Autowired
+	private BCryptPasswordEncoder passEncoder;
+	
+	@Autowired
+	public void cofigurerSecurityGlobal(AuthenticationManagerBuilder builder) throws Exception {
+		builder.jdbcAuthentication()
+		.dataSource(dataSource)
+		.passwordEncoder(passEncoder)
+		.usersByUsernameQuery("SELECT username, password, enable FROM users WHERE username=?")
+		.authoritiesByUsernameQuery("SELECT u.username, r.rol FROM roles r INNER JOIN users u ON r.user_id=u.id WHERE u.username=?");
+	}
+}
