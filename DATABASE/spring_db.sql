dbab18080d9d65838f61083766a051f2c2288fb7
MkPolo <mk.polo1708@gmail.com>
2020-04-27 22:16:50 -0500

    Se agregó el README.MD y la base de datos

---
+++ DATABASE/spring_db.sql
@@ -0,0 +1,135 @@
+﻿-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1:3306
+-- Tiempo de generación: 28-04-2020 a las 03:13:05
+-- Versión del servidor: 5.7.24
+-- Versión de PHP: 7.2.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `spring_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ciudades`
+--
+
+DROP TABLE IF EXISTS `ciudades`;
+CREATE TABLE IF NOT EXISTS `ciudades` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `ciudad` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `ciudades`
+--
+
+INSERT INTO `ciudades` (`id`, `ciudad`) VALUES
+(1, 'Lima'),
+(2, 'San Isidro');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `clientes`
+--
+
+DROP TABLE IF EXISTS `clientes`;
+CREATE TABLE IF NOT EXISTS `clientes` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `apellidos` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `nombres` varchar(255) DEFAULT NULL,
+  `telefono` varchar(255) DEFAULT NULL,
+  `ciudades_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKaf22wxd0vy9h69nta0p35jses` (`ciudades_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `clientes`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `roles`
+--
+
+DROP TABLE IF EXISTS `roles`;
+CREATE TABLE IF NOT EXISTS `roles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `rol` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user_id` (`user_id`,`rol`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `roles`
+--
+
+INSERT INTO `roles` (`id`, `user_id`, `rol`) VALUES
+(1, 1, 'ROLE_USER'),
+(2, 2, 'ROLE-USER'),
+(3, 2, 'ROLE_ADMIN');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) NOT NULL,
+  `password` varchar(60) NOT NULL,
+  `enable` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `enable`) VALUES
+(1, 'user', '$2a$10$O/kUKmMiMIBu/OqixoKj7OMqK0UlAHHSqG9ehHqBPVKrNx.LnbcXO', 1),
+(2, 'admin', '$2a$10$h52CVys5B2Q7N39gTEsIROpAtzat.zWKN4DnRLbqZRN1O7UCN2HwK', 1);
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  ADD CONSTRAINT `FKaf22wxd0vy9h69nta0p35jses` FOREIGN KEY (`ciudades_id`) REFERENCES `ciudades` (`id`);
+
+--
+-- Filtros para la tabla `roles`
+--
+ALTER TABLE `roles`
+  ADD CONSTRAINT `roles_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ README.md
@@ -0,0 +1,16 @@
+# SPRING SECURITY
+## MODULOS
+[[Introduccion](#introduccion)]
+[[Base de Datos](#database)]
+[[Interfaz](#Interfaz)]
+# Introduccion
+* Este proyecto es la continuacion del anterior CRUD, ahora estaremos implementando Spring Security.
+* Crearemos un formulario Login con las validaciones correspondientes, tambien validaremos las acciones con diferentes ROLES para cada usuario.
+# Database
+* Estaremos utilizando **MySQL** como gestor de base de datos.
+* [Esta es la base de datos](DATABASE/spring_db.sql)
+* Las tablas serán creadas utilizando **HIBERNATE**.
+# Interfaz
+* Para las interfaces estaremos utilizando: **Thymeleaf** y [**Bootstrap**](https://getbootstrap.com/).
+* Utilizaremos mensajes personalizados para cada acción realizada.
+* Validaremos los errores 400, 403 y 404.
